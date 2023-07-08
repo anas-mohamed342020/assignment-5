2adcba911d8a1270492f3a76243ee26d83299a77
anas-mohamed342020 <ans192174@fci.bu.edu.eg>
2023-07-08 16:37:49 +0300

    change DB

--- DB/connection.js
+++ DB/connection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 
 export const connection = async () => {
-    return await mongoose.connect('mongodb+srv://anas:GngZM3HOG6Es5enq@cluster0.kwnfpnh.mongodb.net/assignment5').then(() => {
+    return await mongoose.connect('mongodb+srv://Anas:Eng_anas_121201101459565@cluster0.gkhmpi7.mongodb.net/assignment5?retryWrites=true&w=majority').then(() => {
         console.log("DB connected");
     }).catch(err => {
         console.log({ message: "failed to connect DB", err });
