5306a6db4249fd1e68eb5d34d1c8de28eb854047
anas-mohamed342020 <ans192174@fci.bu.edu.eg>
2023-07-08 16:19:23 +0300

    re upload

--- DB/connection.js
+++ DB/connection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 
 export const connection = async () => {
-    return await mongoose.connect('mongodb://0.0.0.0:27017/assignment5').then(() => {
+    return await mongoose.connect('mongodb+srv://anas:GngZM3HOG6Es5enq@cluster0.kwnfpnh.mongodb.net/assignment5').then(() => {
         console.log("DB connected");
     }).catch(err => {
         console.log({ message: "failed to connect DB", err });
